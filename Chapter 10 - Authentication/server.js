f5c6c94ae60c9e96ba740b5f3efed0aab0b562a0
Werald Opolento <werald.opolento@gmail.com>
2024-08-26 04:37:16 +0800

    added Chapter 10 the Authentication, i love login and registration process in program. but i need to understand this deeply.

---
+++ Chapter 10 - Authentication/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const { logger, logEvents } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const corsOptions = require('./config/corsOptions');
+const PORT = process.env.PORT || 3500;
+
+// Custom middleware
+app.use(logger);
+
+// Allowed all the site from whitelist to access api from your site.
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: false }));
+
+// built in middleware for json
+app.use(express.json());
+
+// serve static files
+// express.static('folder path')
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/root'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/employees', require('./routes/api/employees'));
+
+app.all('*', (req, res) => {
+	res.status(404);
+	if (req.accepts('html')) {
+		res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+	} else if (req.accepts('json')) {
+		res.json({ error: '404 Not found!' });
+	} else {
+		res.type('txt').send('404 Not found!');
+	}
+});
+
+app.use(errorHandler);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
