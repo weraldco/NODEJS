e5e35969eedb7b42d08d77605ade2f9fc3d511df
Werald Opolento <werald.opolento@gmail.com>
2024-08-26 12:30:17 +0800

    m

---
+++ Chapter 12 - User roles/controllers/logoutController.js
@@ -0,0 +1,34 @@
+const usersDB = {
+	users: require('../model/users.json'),
+	setUsers: function (data) {
+		this.users = data;
+	},
+};
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const handleLogout = async (req, res) => {
+	const cookies = req.cookies;
+	if (!cookies?.jwt) return res.sendStatus(204);
+	const refreshToken = cookies.jwt;
+
+	// Is refresh token in db or in data?
+	const foundUser = usersDB.users.find((u) => u.refreshToken === refreshToken);
+	if (!foundUser) {
+		res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+		return res.sendStatus(204);
+	}
+	const otherUsers = usersDB.users.filter(
+		(u) => u.refreshToken !== foundUser.refreshToken
+	);
+	const currentUser = { ...foundUser, refreshToken: '' };
+	usersDB.setUsers([...otherUsers, currentUser]);
+	await fsPromises.writeFile(
+		path.join(__dirname, '..', 'model', 'users.json'),
+		JSON.stringify(usersDB.users)
+	);
+	res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+	res.sendStatus(204);
+};
+
+module.exports = { handleLogout };
---
+++ Chapter 12 - User roles/controllers/registerController.js
@@ -0,0 +1,44 @@
+const usersDB = {
+	users: require('../model/users.json'),
+	setUsers: function (data) {
+		this.users = data;
+	},
+};
+
+const fsPromise = require('fs').promises;
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async (req, res) => {
+	const { user, pwd } = req.body;
+	if (!user || !pwd) {
+		return res
+			.status(400)
+			.json({ message: `Error: username and password are required` });
+	}
+	const duplicate = usersDB.users.find((u) => u.username === user);
+	if (duplicate) return res.sendStatus(409);
+
+	try {
+		const hashPwd = await bcrypt.hash(pwd, 10);
+		const newUser = {
+			username: user,
+			roles: {
+				User: '2001',
+			},
+			password: hashPwd,
+		};
+		usersDB.setUsers([...usersDB.users, newUser]);
+
+		await fsPromise.writeFile(
+			path.join(__dirname, '..', 'model', 'users.json'),
+			JSON.stringify(usersDB.users)
+		);
+		console.log(usersDB.users);
+		res.status(201).json({ message: `New user ${user} has been registered.` });
+	} catch (err) {
+		res.status(500).json({ message: `${err.message}` });
+	}
+};
+
+module.exports = { handleNewUser };
