a98258251322a9068d09acde5a55376f7b0d9d02
Werald Opolento <werald.opolento@gmail.com>
2024-08-27 08:12:09 +0800

    Chapter 12 done, need to polish my understanding

--- Chapter 12 - User roles/middleware/verifyJWT.js
+++ Chapter 12 - User roles/middleware/verifyJWT.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const verifyJWT = (req, res, next) => {
 	const authHeader = req.headers.authorization || req.headers.Authorization;
-	if (!authHeader?.startWith('Bearer ')) return res.sendStatus(401);
+	if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
 	const token = authHeader.split(' ')[1];
 
 	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
